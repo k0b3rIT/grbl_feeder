649feffe1382c309b04308cf44d78ad241b852f8
k0b3rit <kb.pcre@gmail.com>
2023-01-04 17:46:57 +0100

    scale

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -12,10 +12,18 @@ import (
 func main() {
 	var grbl *grblfeeder.GrblFeeder = grblfeeder.NewGrblFeeder("/dev/ttyUSB0")
 	gcode := util.ReadFile("lilien.gcode")
-	modifiedGcode := gcodemanipulator.MoveToTopLeft(gcode, 305, 200)
+	modifiedGcode := gcodemanipulator.ScaleTo(gcode, 50)
+	modifiedGcode = gcodemanipulator.MoveToTopLeft(modifiedGcode, 305, 200)
+	grbl.SendGcodes(modifiedGcode)
+
+	modifiedGcode = gcodemanipulator.OffsetX(modifiedGcode, 60)
+	modifiedGcode = gcodemanipulator.OffsetY(modifiedGcode, -60)
+	grbl.SendGcodes(modifiedGcode)
+
+
 	// fmt.Println(modifiedGcode)
 
-	util.WriteFile("m_lilien.gcode", modifiedGcode)
+	// util.WriteFile("m_lilien.gcode", modifiedGcode)
 
-	grbl.SendGcodes(modifiedGcode)
+	
 }
--- pkg/gcode_manipulator/manipulator.go
+++ pkg/gcode_manipulator/manipulator.go
@@ -2,14 +2,21 @@ package gcodemanipulator
 
 import (
 	"fmt"
+	"math"
 	"regexp"
 	"strconv"
 	"strings"
 )
 
+	
+type gcode struct {
+    symbol string
+    pattern *regexp.Regexp
+}
+
 var (
-	xValueRegex = regexp.MustCompile(`X(-*\d+(?:\.\d+|\d*))`)	
-	yValueRegex = regexp.MustCompile(`Y(-*\d+(?:\.\d+|\d*))`)
+	GCODE_X = gcode{"X", regexp.MustCompile(`X(-*\d+(?:\.\d+|\d*))`)}
+	GCODE_Y = gcode{"Y", regexp.MustCompile(`Y(-*\d+(?:\.\d+|\d*))`)}
 )
 
 func MoveToTopLeft(gcode []string, machineMaxX float64, machineMaxY float64) []string {
@@ -19,15 +26,12 @@ func MoveToTopLeft(gcode []string, machineMaxX float64, machineMaxY float64) []s
 
 	offset := machineMaxY - maxY
 
-
 	var modifiedGcode = []string{}
 
 	for _, line := range gcode {
-		originalValue := yValueRegex.FindStringSubmatch(line)
-		if (len(originalValue) > 0) {
-			yValue, _ := strconv.ParseFloat(originalValue[1], 64)
-			newLine := strings.Replace(line, originalValue[0], fmt.Sprintf("Y%.4f", yValue + offset), 1)
-			// fmt.Println(line + "\t\t" + newLine)
+		yValue, err := getValueFromGcodeLine(line, GCODE_Y)
+		if (err == nil) {
+			newLine := replaceValueInGcodeLine(line, GCODE_Y, yValue + offset)
 			modifiedGcode = append(modifiedGcode, newLine)
 		} else {
 			modifiedGcode = append(modifiedGcode, line)
@@ -36,41 +40,105 @@ func MoveToTopLeft(gcode []string, machineMaxX float64, machineMaxY float64) []s
 	return modifiedGcode
 }
 
+func ScaleTo(gcode []string, maxWidthAndHight float64) []string {
+	minX, maxX, minY, maxY := getGcodeExtremes(gcode)
+	width := maxX - minX
+	height := maxY - minY
+
+	if (height > maxWidthAndHight || width > maxWidthAndHight) {
+		largestDimension := math.Max(height, width)
+		scaleFactor := largestDimension / maxWidthAndHight
+
+		var modifiedGcode = []string{}
+
+		for _, line := range gcode {
+			lineTmp := line
+			yValue, err := getValueFromGcodeLine(lineTmp, GCODE_Y)
+			if (err == nil) {
+				lineTmp = replaceValueInGcodeLine(lineTmp, GCODE_Y, yValue / scaleFactor)
+			}
+
+			xValue, err := getValueFromGcodeLine(lineTmp, GCODE_X)
+			if (err == nil) {
+				lineTmp = replaceValueInGcodeLine(lineTmp, GCODE_X, xValue / scaleFactor)
+			}
+
+			modifiedGcode = append(modifiedGcode, lineTmp)
+		}
+
+		return modifiedGcode
+	}
+
+	return gcode
+}
+
+func OffsetX(gcode []string, offset float64) []string {
+	return offsetOnGcode(GCODE_X, gcode, offset)
+}
+
+func OffsetY(gcode []string, offset float64) []string {
+	return offsetOnGcode(GCODE_Y, gcode, offset)
+}
+
+func offsetOnGcode(gcodeType gcode, gcode []string, offset float64) []string {
+	var modifiedGcode = []string{}
+	for _, line := range gcode {
+		value, err := getValueFromGcodeLine(line, gcodeType)
+		if (err == nil) {
+			newLine := replaceValueInGcodeLine(line, gcodeType, value + offset)
+			modifiedGcode = append(modifiedGcode, newLine)
+		} else {
+			modifiedGcode = append(modifiedGcode, line)
+		}
+	}
+	return modifiedGcode
+}
+
+func getValueFromGcodeLine(line string, gcodeType gcode) (float64, error) {
+	values := gcodeType.pattern.FindStringSubmatch(line)
+	if (len(values) > 0) {
+		value, _ := strconv.ParseFloat(values[1], 64)
+		return value, nil
+	} else {
+		return 0, fmt.Errorf("no [%s] value found", gcodeType.symbol)
+	}
+}
+
+func replaceValueInGcodeLine(line string, gcodeType gcode, newValue float64) string {
+	originalValue := gcodeType.pattern.FindStringSubmatch(line)
+	if (len(originalValue) > 0) {
+		newLine := strings.Replace(line, originalValue[0], fmt.Sprintf("%s%.4f", gcodeType.symbol, newValue), 1)
+		return newLine
+	} else {
+		panic(fmt.Sprintf("Gcode [%s] not found!", gcodeType.symbol))
+	}
+}
+
 func getGcodeExtremes(gcode []string) (float64, float64, float64, float64) {
 	var minX float64 = 0
 	var maxX float64 = 0
 	var minY float64 = 0
 	var maxY float64 = 0
 	for _, line := range gcode {
-		xStringParts := xValueRegex.FindStringSubmatch(line)
-		if (len(xStringParts) > 0) {
-			value, _ := strconv.ParseFloat(xStringParts[1], 64)
-			if (minX > value) {
-				minX = value
+		xValue, err := getValueFromGcodeLine(line, GCODE_X)
+		if (err == nil) {
+			if (minX > xValue) {
+				minX = xValue
 			}
-			if (maxX < value) {
-				maxX = value
-			}
-		}
-
-		yStringParts := yValueRegex.FindStringSubmatch(line)
-		if (len(yStringParts) > 0) {
-			fmt.Println(yStringParts[1])
-			value, _ := strconv.ParseFloat(yStringParts[1], 64)
-			if (minY > value) {
-				minY = value
-			}
-			if (maxY < value) {
-				maxY = value
+			if (maxX < xValue) {
+				maxX = xValue
 			}
 		}
 
-
+		yValue, err := getValueFromGcodeLine(line, GCODE_Y)
+		if (err == nil) {
+			if (minY > yValue) {
+				minY = yValue
+			}
+			if (maxY < yValue) {
+				maxY = yValue
+			}
+		}
 	}
 	return minX, maxX, minY, maxY
-}
-
-
-func getGcodeMaxY(gcode []string) float32 {
-	return 0
 }
